37aa725fea9e28750b80d5acef559690404c405b
Kenny Ayers <theotherwhitemeat@gmail.com>
2013-05-08 23:52:41 -0700

    Oye, how did those tabs get in there?

--- AdvertisingAgency/AdvertisingAgency.py
+++ AdvertisingAgency/AdvertisingAgency.py
@@ -4,7 +4,7 @@ Problem Statement for AdvertisingAgency
 
 
 Problem Statement
-    	
+        
 You are working in an advertising agency. There are 100 billboards owned by your agency, numbered from 1 to 100.
 
 You clients send you requests, one after another. Each request is the number of the billboard on which the client would like to place his advertisement.
@@ -15,36 +15,36 @@ You are given a int[] requests containing the requests in the order you receive 
 
  
 Definition
-    	
-Class:	AdvertisingAgency
-Method:	numberOfRejections
-Parameters:	int[]
-Returns:	int
-Method signature:	int numberOfRejections(int[] requests)
+        
+Class:    AdvertisingAgency
+Method:    numberOfRejections
+Parameters:    int[]
+Returns:    int
+Method signature:    int numberOfRejections(int[] requests)
 (be sure your method is public)
     
  
 Constraints
--	requests will contain between 1 and 50 elements, inclusive.
--	Each element of requests will be between 1 and 100, inclusive.
+-    requests will contain between 1 and 50 elements, inclusive.
+-    Each element of requests will be between 1 and 100, inclusive.
  
 Examples
-0)	
-    	
+0)    
+        
 {1,2,3}
 Returns: 0
 All requests will be satisfied.
-1)	
-    	
+1)    
+        
 {1,1,1}
 Returns: 2
 Only the first request will be satisfied.
-2)	
-    	
+2)    
+        
 {1,2,1,2}
 Returns: 2
-3)	
-    	
+3)    
+        
 {100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
  100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
  100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
@@ -55,35 +55,36 @@ Returns: 49
 """
 
 class AdvertisingAgency(object):
-	def __init__():
-		pass
+    def __init__():
+        pass
 
-	@staticmethod
-	def numberOfRejections(requests):
-		""" (int) returns the number of rejected requests """
+    @staticmethod
+    def numberOfRejections(requests):
+        """ (int) returns the number of rejected requests """
 
-		rejects = 0
-		billboards = dict()
-		for request in requests:
-			if billboards.get(request, None) is None:
-				billboards[request] = True
-			else:
-				rejects += 1
+        rejects = 0
+        billboards = dict()
+        for request in requests:
+            if billboards.get(request, None) is None:
+                billboards[request] = True
+            else:
+                rejects += 1
 
-		return rejects
+        return rejects
 
 def main():
-	samples = [[1,2,3],
-			  [1,1,1],
-			  [1,2,1,2],
-			  [100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-				 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-				 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-				 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-				 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],]
+    samples = [[1,2,3],
+              [1,1,1],
+              [1,2,1,2],
+              [100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+               100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+               100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+               100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+               100, 100, 100, 100, 100, 100, 100, 100, 100, 100],]
 
-	for sample in samples:
-		print "Sample %s has %d rejects" % (sample, AdvertisingAgency.numberOfRejections(sample))
+    for sample in samples:
+        print "Sample %s has %d rejects" % (sample, AdvertisingAgency.numberOfRejections(sample))
 
 if __name__ == "__main__":
-	main()
+    main()
+
