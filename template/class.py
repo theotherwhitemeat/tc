aa8e7f79e49c20b78092b8f0219aa941d29fbc2c
Kenny Ayers <theotherwhitemeat@gmail.com>
2013-05-09 17:49:14 -0700

    Adding problem template

---
+++ template/class.py
@@ -0,0 +1,20 @@
+"""
+http://community.topcoder.com/stat?c=problem_statement
+
+
+Status: WIP
+"""
+
+class UserName(object):
+    def __init__(self):
+        pass
+
+def main():
+    """ sup main """
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+
