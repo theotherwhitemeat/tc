12370b18e7dfcf3fc11a7bd3dd0c49510189e30a
Kenny Ayers <theotherwhitemeat@gmail.com>
2013-06-01 01:16:32 -0700

    Adding functional LargestSubsequence.  My previous strategy turned out to be O(2^n), and was a fundamentally bad idea.  This implementation runs in O(nlogn), bounded by the PriorityQueue heap sort.

--- LargestSubsequence/LargestSubsequence.py
+++ LargestSubsequence/LargestSubsequence.py
@@ -45,8 +45,8 @@ Returns: "xple"
 "aquickbrownfoxjumpsoverthelazydog"
 Returns: "zyog"
 
-Status: WIP
-Runtime: 
+Status: Complete
+Runtime: O(nlogn)
 """
 
 class LargestSubsequence(object):
@@ -58,25 +58,96 @@ class LargestSubsequence(object):
         """ (str()) Given string s, return the lexicographically largest
             subsequence. """
 
-        #Strategy: generate all substrings, then rank them
+        # Strategy
+        #
+        # Discussion:
+        #  This method discovers the largest possible substring in a 
+        #  given string.  The criteria are subtle, and the problem
+        #  statement above must be read to understand this implementation.
+        #
+        #  The largest substring in string (s) is built by finding the largest
+        #  lexico character (x) in string (s) iterating from left to right,
+        #  and then the largest lexico character after (x), and so on.  For
+        #  'abc' the largest character is the last, thus the largest substring
+        #  is simply 'c'.  For 'zabcz', the largest lexico characters are the
+        #  first and last, and thus the largest substring is 'zz'. Finally for
+        #  string 'cba', the largest characters are in order, and thus the
+        #  largest substring is 'cba'.
+        #
+        # Implementation:
+        #  1) Push chars to priority queue, then pop into a sorted list.
+        #  2a) Iterate through characters and append each character
+        #   that is different from the previous and has a larger index.
+        #  2b) Handle cases where the same character repeats multiple
+        #   times and may or may not have an appropriate index to append.
 
-        def generateSubstrings(string_base):
-            """ () Given a string base, generate all possible substrings. """
+        import Queue 
+        import sys
 
-            # Strategy: creating a counting bit mask to reference string 
-            #  positions, and use that to generate the substrings, along with
-            #  a score based upon a left-aligned string value:
-            #  abc
-            #  001: (c, 300)
-            #  010: (b, 200)
-            #  011: (bc, 230)
+        char_q = Queue.PriorityQueue()
+        prioritized_chars = list()
+        output = str()
+        last_index = -1
+        last_char = str()
+        dupe_count = int()
+        dupe_max = int()
+        first_run = True
 
-            for i in range(len(string_base)):
-                print bin(i)
+        # Push items to priority queue
+        for i, char in enumerate(s):
+            char_q.put((char, i))
 
-        generateSubstrings(s)
+        # Pop items off of priority queue to sort them
+        while char_q.qsize() > 0:
+            prioritized_chars.append(char_q.get())
 
-        
+        # Iterate through chars in order of largest first
+        for next_char, next_index in prioritized_chars[::-1]:
+            # Setup initial run characters
+            if first_run:
+                char = next_char
+                index = next_index
+                first_run = False
+            else:
+                # If the next char is different...
+                if char != next_char:
+                    # If this char and the last are the same, increase dupes
+                    if (char == last_char) and (index > last_index):
+                        dupe_count += 1
+                    # If we have dupes, append them to output
+                    if dupe_count > 0:
+                        output += dupe_count * char
+                        dupe_count = 0
+                        last_index = dupe_max
+                    # Else if this char isn't a dupe of the previous nor the
+                    #  next, so it is unique and we may append it. Only append
+                    #  if the index is larger than the previous, append it.
+                    elif index > last_index:
+                        output += char
+                        last_index = index
+                # If this char and the next char are the same...
+                else:
+                    # If this duplicate has a larger index, increase dupe count
+                    if index > last_index:
+                        print "Adding", 
+                        dupe_count += 1
+                        dupe_max = max(index, dupe_max)
+                # Housekeeping: update last_char, char, index
+                last_char = char
+                char = next_char
+                index = next_index
+        # Handle the final case: if dupe, increase count
+        if (char == last_char) and (index > last_index):
+            dupe_count += 1
+        # If dupe, append apropos chars
+        if dupe_count > 0:
+            output += dupe_count * char
+        # If this isn't a dupe, and has a higher index, append char
+        if (char != last_char) and (index > last_index):
+            output += char
+
+        return output
+
 
 def main():
     """ sup main """
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Completed
 
 ## Division I, Level One ##
 - AdvertisingAgency:     http://community.topcoder.com/stat?c=problem_statement&pm=7558
+- LargestSubsequence:    http://community.topcoder.com/stat?c=problem_statement&pm=11471
 - MatchMaking:           http://community.topcoder.com/stat?c=problem_statement&pm=2911
 - PalindromeDecoding:    http://community.topcoder.com/stat?c=problem_statement&pm=6767
 - PaperFold:             http://community.topcoder.com/stat?c=problem_statement&pm=1846
@@ -44,4 +45,3 @@ WIP
 
 ## Division I, Level One ##
 - Alignment:             http://community.topcoder.com/stat?c=problem_statement&pm=6249
-- LargestSubsequence:    http://community.topcoder.com/stat?c=problem_statement&pm=11471
